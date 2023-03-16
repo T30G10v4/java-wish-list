196cb2d8ae64cfdc13e707423148e66523234fb9
T30G10v4 <matteo.giovagnotti@gmail.com>
2023-03-16 14:50:43 +0100

    ADD: second exercise

---
+++ src/org/lessons/java/christmas/Main.java
@@ -0,0 +1,41 @@
+package org.lessons.java.christmas;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        List<String> wishes = new ArrayList<>();
+
+        String choose = "";
+        String wish = "";
+
+
+        do {
+
+            System.out.print("Inserisci un nuovo desiderio per Natale: ");
+            wish = scan.nextLine();
+            wishes.add(wish);
+            System.out.print("Vuoi inserire un nuovo desiderio? S/N: ");
+            choose = scan.nextLine();
+
+        } while (!choose.equalsIgnoreCase("n"));
+
+
+        Collections.sort(wishes);
+
+        ChristmasLetter letter1 = new ChristmasLetter(" Luigino ", " via Milano 144 ", wishes);
+        System.out.println(letter1.isGood());
+        try {
+            letter1.send();
+        } catch (RuntimeException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        //System.out.println(wishes.toString());
+
+    }
+}
